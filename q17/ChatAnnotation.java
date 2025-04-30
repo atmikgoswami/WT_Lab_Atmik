edd496210b1ddf11d3b8ee7b002ec4a0d0f4b830
Atmik Goswami <atmikcodes@gmail.com>
2025-04-30 13:23:06 +0530

    fix

--- q17/ChatAnnotation.java
+++ q17/ChatAnnotation.java
@@ -27,6 +27,7 @@ public class ChatAnnotation {
     private static final String GUEST_PREFIX = "Guest";
     private static final AtomicInteger connectionIds = new AtomicInteger(0);
     private static final Set<ChatAnnotation> connections = new CopyOnWriteArraySet<>();
+    private static final Set<String> activeNicknames = new CopyOnWriteArraySet<>();
 
     private final String nickname;
     private Session session;
@@ -41,25 +42,33 @@ public class ChatAnnotation {
     public void start(Session session) {
         this.session = session;
         connections.add(this);
+        activeNicknames.add(nickname);  
         String message = String.format("* %s %s", nickname, "has joined.");
         broadcast(message);
     }
 
-
     @OnClose
     public void end() {
         connections.remove(this);
+        activeNicknames.remove(nickname);  
         String message = String.format("* %s %s", nickname, "has disconnected.");
         broadcast(message);
     }
 
-
     @OnMessage
     public void incoming(String message) {
-        String filteredMessage = String.format("%s: %s", nickname, HTMLFilter.filter(message.toString()));
-        broadcast(filteredMessage);
+        if (message.startsWith("To:")) {
+            String[] parts = message.split(" ", 2);
+            if (parts.length > 1) {
+                String recipientNickname = parts[0].substring(3);  
+                String privateMessage = parts[1];
+                sendPrivateMessage(recipientNickname, privateMessage);
+            }
+        } else {
+            String filteredMessage = String.format("%s: %s", nickname, HTMLFilter.filter(message));
+            broadcast(filteredMessage);
+        }
     }
-
 
     @OnError
     public void onError(Throwable t) throws Throwable {
@@ -67,7 +76,6 @@ public class ChatAnnotation {
     }
 
     private void sendMessage(String msg) throws IOException {
-
         synchronized (this) {
             if (messageInProgress) {
                 messageBacklog.add(msg);
@@ -91,8 +99,27 @@ public class ChatAnnotation {
             }
 
         } while (queueHasMessagesToBeSent);
-     }
+    }
 
+    private void sendPrivateMessage(String recipientNickname, String message) {
+        for (ChatAnnotation client : connections) {
+            if (client.nickname.equals(recipientNickname)) {
+                try {
+                    String privateMessage = String.format("Private from %s: %s", nickname, message);
+                    client.sendMessage(privateMessage);
+                    this.sendMessage(String.format("Private to %s: %s", recipientNickname, message));
+                    return;
+                } catch (IOException e) {
+                    log.error("Failed to send private message", e);
+                }
+            }
+        }
+        try {
+            this.sendMessage(String.format("User %s not found", recipientNickname));
+        } catch (IOException e) {
+            log.error("Failed to send error message", e);
+        }
+    }
 
     private static void broadcast(String msg) {
         for (ChatAnnotation client : connections) {
